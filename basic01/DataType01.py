ca8f6a2614c8b396f365c377e9ce6bfe6ce15e17
answlgus1235 <klkjjj4848@hanmail.net>
2022-04-13 23:07:55 +0900

    1. Numeric Types

---
+++ basic01/DataType01.py
@@ -0,0 +1,33 @@
+a = 10
+b = 5
+
+print (a+b)
+print (a-b)
+print (a ** b)
+
+a = 10.3 
+b = -1.6
+
+print (a + b)
+print (a - b)
+print (a * b)
+print (a / b)
+
+#특수연산
+c = 5
+d = 2
+
+print(c ** d) #c를 d번 곱한 것 
+print(c // d) #몫
+print(c % d) #나머지
+
+#문제 
+N = 2000
+if N % 4 == 0 and N % 100 != 0 or N % 400 == 0  :
+    print ("true")
+    
+else : 
+    print ("false")
+    
+
+
